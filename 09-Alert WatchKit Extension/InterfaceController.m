68e134f00b04fd797dc8ac4b809f50b2acea3e00
TimZInGitHub <Tim@11234.local>
2015-12-02 16:10:03 +0800

    15资料-装逼特技(蓝牙+applewatch)-1010-appleWatch(2)-09-Alert
    15资料-装逼特技(蓝牙+applewatch)-1010-appleWatch(2)-09-Alert

--- 09-Alert WatchKit Extension/InterfaceController.m
+++ 09-Alert WatchKit Extension/InterfaceController.m
@@ -15,6 +15,69 @@
 
 
 @implementation InterfaceController
+- (IBAction)alertShow {
+    
+    WKAlertAction *a0 = [WKAlertAction actionWithTitle:@"a0T-cancle"
+                                                 style:WKAlertActionStyleCancel handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    WKAlertAction *a1 = [WKAlertAction actionWithTitle:@"a1T-def"
+                                                 style:WKAlertActionStyleDefault handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    
+    WKAlertAction *a2 = [WKAlertAction actionWithTitle:@"a2T-destr"
+                                                 style:WKAlertActionStyleDestructive handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    [self presentAlertControllerWithTitle:@"title0"
+                                  message:@"xmgALertMessage"
+                           preferredStyle:WKAlertControllerStyleAlert
+                                  actions:@[a0, a1, a2]];
+}
+
+// 它与上面的不同只是取消跑到了上面
+- (IBAction)actionSheetShow {
+    WKAlertAction *a0 = [WKAlertAction actionWithTitle:@"a0T-cancle"
+                                                 style:WKAlertActionStyleCancel handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    WKAlertAction *a1 = [WKAlertAction actionWithTitle:@"a1T-def"
+                                                 style:WKAlertActionStyleDefault handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    
+    WKAlertAction *a2 = [WKAlertAction actionWithTitle:@"a2T-destr"
+                                                 style:WKAlertActionStyleDestructive handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    [self presentAlertControllerWithTitle:@"title0"
+                                  message:@"xmgALertMessage"
+                           preferredStyle:WKAlertControllerStyleActionSheet
+                                  actions:@[a0, a1, a2]];
+
+}
+
+// WKAlertControllerStyleSideBySideButtonsAlert这种样式只允许添加2个action,他们是横排的
+- (IBAction)showA3 {
+    WKAlertAction *a0 = [WKAlertAction actionWithTitle:@"a0T-cancle"
+                                                 style:WKAlertActionStyleCancel handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    WKAlertAction *a1 = [WKAlertAction actionWithTitle:@"a1T-def"
+                                                 style:WKAlertActionStyleDefault handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    
+    WKAlertAction *a2 = [WKAlertAction actionWithTitle:@"a2T-destr"
+                                                 style:WKAlertActionStyleDestructive handler:^{
+                                                     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+                                                 }];
+    [self presentAlertControllerWithTitle:@"title0"
+                                  message:@"xmgALertMessage"
+                           preferredStyle:WKAlertControllerStyleSideBySideButtonsAlert
+                                  actions:@[a0, a2]];
+}
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
